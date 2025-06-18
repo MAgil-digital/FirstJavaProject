9b25424b945dc84be46136abc331ef2baa01f8c6
MAgil-digital <MAgil.network@gmail.com>
2025-06-19 00:16:36 +0200

    Created new File: Variables.java where we use variables for the first time

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args){
+        //Variables are container for Values, Variables do have different types
+
+        int number = 3;
+        System.out.println(number);
+    }
+}
