66ae6ea05d5d32cf0f6f5e82fb4ac40a33018a08
MAgil-digital <MAgil.network@gmail.com>
2025-06-19 00:36:55 +0200

    I tried out all int, double, string and boolean data types

--- src/Variables.java
+++ src/Variables.java
@@ -3,6 +3,27 @@ public class Variables {
         //Variables are container for Values, Variables do have different types
 
         int number = 3;
-        System.out.println(number);
+        int number2 = 14;
+        int sum = number + number2;
+
+        System.out.println(sum);
+
+        String text = "Das ist ein Text: ";
+
+        System.out.println(text);
+
+        System.out.println(text + number2);
+
+        double Kommazahl = 14.0433;
+
+        System.out.println(Kommazahl);
+
+        boolean bool = false;
+
+        if(Kommazahl < 15.00){
+            bool = true;
+        }
+
+        System.out.println(bool);
     }
 }
